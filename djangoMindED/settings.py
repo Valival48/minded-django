8a0c58952692d83125416e55f2b73614cb2bb6eb
Valival48 <72327511+Valival48@users.noreply.github.com>
2023-11-13 01:40:18 -0500

    images

--- djangoMindED/settings.py
+++ djangoMindED/settings.py
@@ -51,11 +51,13 @@ INSTALLED_APPS = [
 
 STATIC_URL='static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'MindED/static'),)
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -137,6 +139,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'MindED/static'),)
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
