fe91c416f25a46afb88523e82e146c5f42876c72
Valival48 <72327511+Valival48@users.noreply.github.com>
2023-11-12 22:54:58 -0500

    settings

--- djangoMindED/settings.py
+++ djangoMindED/settings.py
@@ -44,9 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'administracion.apps.AdministracionConfig',
-    'library.apps.LibraryConfig',
-    'users.apps.UsersConfig',
     'MindED',
     'azureml'
     ]
